bfa6710fb2542f5d2ce415adc42ca560aa384801
ghzamani <ghzamaninejad@gmail.com>
2019-10-12 18:36:36 +0330

    A3 solved

---
+++ A3/A3/Q2FibonacciFast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TestCommon;
+
+namespace A3
+{
+    public class Q2FibonacciFast : Processor
+    {
+        public Q2FibonacciFast(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long>)Solve);
+
+        public long Solve(long n)
+        {
+			long[] fib = new long[n + 2];
+			fib[0] = 0;
+			fib[1] = 1;
+
+			for(int i = 2; i <= n; i++)
+			{
+				fib[i] = fib[i - 1] + fib[i - 2];
+			}
+			return fib[n];
+        }
+    }
+}
---
+++ A3/A3/Q9FibonacciSumSquares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TestCommon;
+
+namespace A3
+{
+    public class Q9FibonacciSumSquares : Processor
+    {
+        public Q9FibonacciSumSquares(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long>)Solve);
+
+        public long Solve(long n)
+        {
+			Q6FibonacciMod fibMod = new Q6FibonacciMod(null);
+
+			long nthFibMod = fibMod.Solve(n, 10);
+			long nMinusOneFibMod = fibMod.Solve(n-1, 10);
+
+			return (nthFibMod + nMinusOneFibMod) * nthFibMod % 10;			
+        }
+    }
+}
