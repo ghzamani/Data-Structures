906857a0821048d0d38bfd68622ca138d346e5d0
ghzamani <ghzamaninejad@gmail.com>
2019-10-26 09:20:17 +0330

    Exam1c

---
+++ E1c/E1c/Q1Stones.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TestCommon;
+
+namespace E1a
+{
+    public class Q1Stones : Processor
+    {
+        public Q1Stones(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)Solve);
+
+
+        public virtual long Solve(long n, long[] stones)
+        {
+			long sum = 0;
+			for(int i = 0; i < stones.Length; i++)
+			{
+				sum += stones[i];
+
+				if (n == sum)
+					return i + 1;
+
+				if (n > sum)
+				{
+					if (i == stones.Length - 1)
+						return 0;
+
+					if(n < sum + stones[i + 1])
+						return i + 2;
+				}
+
+				if (n < sum)
+					return 1;
+			}
+
+			return 0;
+        }
+    }
+}
---
+++ E1c/E1c/Q4HungryFrog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestCommon;
+
+namespace E1b
+{
+    public class Q4HungryFrog : Processor
+    {
+        public Q4HungryFrog(string testDataName) : base(testDataName)
+        {			
+		}
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long[][], long>)Solve);
+
+		public virtual long Solve(long n, long p, long[][] numbers)
+		{			
+			for (int i = 1; i < n ; i++)
+			{
+				numbers[0][i] = Math.Max(numbers[0][i - 1] + numbers[0][i],
+					numbers[1][i - 1] + numbers[0][i] - p);
+
+				numbers[1][i] = Math.Max(numbers[1][i - 1] + numbers[1][i],
+					numbers[0][i - 1] + numbers[1][i] - p);
+			}
+			return Math.Max(numbers[0][n - 1], numbers[1][n - 1]);
+        }	
+		
+	}
+}
