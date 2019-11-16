17ad1010b1e66d2db8c33bf7ffa0751dc9a50665
ghzamani <ghzamaninejad@gmail.com>
2019-11-16 22:02:37 +0330

    A7

---
+++ A7/A7/Q2PartitioningSouvenirs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace A7
+{
+    public class Q2PartitioningSouvenirs : Processor
+    {
+        public Q2PartitioningSouvenirs(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)Solve);
+
+        public long Solve(long souvenirsCount, long[] souvenirs)
+        {
+			if (souvenirsCount < 3)
+				return 0;
+
+			long sum = 0;
+			foreach (var souvenir in souvenirs)
+				sum += souvenir;
+
+			if (sum % 3 != 0)
+				return 0;
+
+			sum /= 3;	
+			if (IsFound(souvenirs, souvenirsCount - 1, sum, sum, sum, new Dictionary<string, bool>()))
+				return 1;
+
+			return 0;
+        }
+
+		public bool IsFound(long[] nums, long n, long s1, long s2, long s3, Dictionary<string,bool> dic)
+		{
+			if ((s1 == 0) && (s2 == 0) && (s3 == 0))
+				return true;
+			if (n < 0)
+				return false;
+
+			string dicKey = s1.ToString() + s2.ToString() + s3.ToString();
+			if (!dic.ContainsKey(dicKey))
+			{
+				bool b1 = false;
+				if (s1 - nums[n] >= 0)
+					b1 = IsFound(nums, n - 1, s1 - nums[n], s2, s3, dic);
+
+				bool b2 = false;
+				if ((b1 == false) && (s2 - nums[n] >= 0))
+					b2 = IsFound(nums, n - 1, s1, s2 - nums[n], s3, dic);
+
+				bool b3 = false;
+				if ((b1 == false) && (b2 == false) && (s3 - nums[n] >= 0))
+					b3 = IsFound(nums, n - 1, s1, s2, s3 - nums[n], dic);
+
+				dic.Add(dicKey, b1 || b2 || b3);
+			}
+			return dic.GetValueOrDefault(dicKey);
+		}
+	}
+}
---
+++ A7/A7/Q3MaximizingArithmeticExpression.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace A7
+{
+	public class Q3MaximizingArithmeticExpression : Processor
+	{
+		public Q3MaximizingArithmeticExpression(string testDataName) : base(testDataName) { }
+
+		public override string Process(string inStr) =>
+			TestTools.Process(inStr, (Func<string, long>)Solve);
+
+		char[] operations;
+		int[,] Max;
+		int[,] Min;
+		public long Solve(string expression)
+		{
+			operations = new char[expression.Length / 2];
+			int j = 0;
+
+			for (int i = 1; i < expression.Length - 1; i += 2)
+			{
+				operations[j] = expression[i];
+				j++;
+			}
+
+			int arraysLength = expression.Length / 2 + 1;
+			Max = new int[arraysLength, arraysLength];
+			Min = new int[arraysLength, arraysLength];
+
+			j = 0;
+			for (int i = 0; i < arraysLength; i++)
+			{
+				Max[i, i] = Min[i, i] = int.Parse(expression[j].ToString());
+				j += 2;
+			}
+
+			for (int i = 1; i <= arraysLength - 1; i++)
+			{
+				for (int k = 0; k < arraysLength - i; k++)
+				{
+					j = i + k;
+					Tuple<int,int> minAndMax = CalculateMinAndMax(k, j);
+					Min[k, j] = minAndMax.Item1;
+					Max[k, j] = minAndMax.Item2;
+				}
+			}
+			return Max[0, arraysLength - 1];
+		}
+
+		public Tuple<int, int> CalculateMinAndMax(int i, int j)
+		{
+			int min = int.MaxValue;
+			int max = int.MinValue;
+
+			for (int k = i; k <= j - 1; k++)
+			{
+				int a = Operate(Min[i, k], Min[k + 1, j], operations[k]);
+				int b = Operate(Min[i, k], Max[k + 1, j], operations[k]);
+				int c = Operate(Max[i, k], Min[k + 1, j], operations[k]);
+				int d = Operate(Max[i, k], Max[k + 1, j], operations[k]);
+				min = SmallestNum(a, b, c, d, min);
+				max = BiggestNum(a, b, c, d, max);
+			}
+			return Tuple.Create(min, max);
+		}			
+		public int Operate(int a, int b, char o)
+		{
+			switch (o)
+			{
+				case '+':
+					return a + b;
+				case '-':
+					return a - b;
+				case '*':
+					return a * b;
+			}
+			return 0;
+		}
+		public static int SmallestNum(params int[] values) => Enumerable.Min(values);
+		public static int BiggestNum(params int[] values) => Enumerable.Max(values);		
+	}
+}
