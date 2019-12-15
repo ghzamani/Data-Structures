1d9aba0f8e6673619e2ce6e5e86ff0fe904938b9
ghzamani <ghzamaninejad@gmail.com>
2019-12-15 17:46:50 +0330

    A10

---
+++ A10/A10/Q2HashingWithChain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestCommon;
+
+namespace A10
+{
+	public class Q2HashingWithChain : Processor
+	{
+		public Q2HashingWithChain(string testDataName) : base(testDataName) {}
+
+		public override string Process(string inStr) =>
+			TestTools.Process(inStr, (Func<long, string[], string[]>)Solve);
+
+		public List<string>[] Data;
+		public string[] Solve(long bucketCount, string[] commands)
+		{
+			Data = new List<string>[bucketCount];
+			for (int i = 0; i < bucketCount; i++)
+				Data[i] = new List<string>();
+
+			List<string> result = new List<string>();
+			foreach (var cmd in commands)
+			{
+				var toks = cmd.Split();
+				var cmdType = toks[0];
+				var arg = toks[1];
+
+				switch (cmdType)
+				{
+					case "add":
+						Add(arg);
+						break;
+					case "del":
+						Delete(arg);
+						break;
+					case "find":
+						result.Add(Find(arg));
+						break;
+					case "check":
+						result.Add(Check(int.Parse(arg)));
+						break;
+				}
+			}
+			return result.ToArray();
+		}
+
+		public const long BigPrimeNumber = 1000000007;
+		public const long ChosenX = 263;
+
+		public static long PolyHash(
+			string str, int start, int count,
+			long p = BigPrimeNumber, long x = ChosenX)
+		{
+			long hash = 0;
+			for (int i = start + count - 1; i >= start; i--)
+			{
+				checked
+				{
+					hash = (hash * x + str[i]) % p;
+				}
+			}
+			return hash;
+		}
+
+		public void Add(string str)
+		{
+			long hash = PolyHash(str, 0, str.Length) % Data.Length;
+
+			if (Data[hash].Count == 0)
+				Data[hash].Add(str);
+
+			else
+				if(!Data[hash].Contains(str))
+					Data[hash].Insert(0, str);
+		}
+
+		public string Find(string str)
+		{
+			long hash = PolyHash(str, 0, str.Length) % Data.Length;
+
+			for (int i = 0; i < Data[hash].Count; i++)
+			{
+				if (Data[hash][i] == str)
+					return "yes";
+			}
+			return "no";
+		}
+
+		public void Delete(string str)
+		{
+			long hash = PolyHash(str, 0, str.Length) % Data.Length;
+
+			if (Data[hash].Count != 0)
+				if (Data[hash].Contains(str))
+					Data[hash].Remove(str);
+		}
+
+		public string Check(int i)
+		{
+			if (Data[i].Count != 0)
+			{
+				string result = string.Join(" ", Data[i]);
+				return result;
+			}
+
+			return "-";
+		}
+	}
+}
---
+++ A10/A10/Q3RabinKarp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace A10
+{
+	public class Q3RabinKarp : Processor
+	{
+		public Q3RabinKarp(string testDataName) : base(testDataName) { }
+
+		public override string Process(string inStr) =>
+			TestTools.Process(inStr, (Func<string, string, long[]>)Solve);
+
+		public long[] Solve(string pattern, string text)
+		{
+			long p = Q2HashingWithChain.BigPrimeNumber;
+			long x = 263;//new Random().Next(1, (int)p - 1);
+			long patternHash = Q2HashingWithChain.PolyHash(pattern, 0, pattern.Length, p, x);
+
+			List<long> positions = new List<long>();
+			long[] H = PreComputeHashes(text, pattern.Length, p, x);
+			for (int i = 0; i <= text.Length - pattern.Length; i++)
+			{
+				if (patternHash != H[i])
+					continue;
+
+				StringBuilder sb = new StringBuilder(pattern.Length);
+				for (int j = i; j <= i + pattern.Length - 1; j++)
+					sb.Append(text[j]);
+				if (sb.ToString() == pattern)
+					positions.Add(i);
+			}
+			return positions.ToArray();
+		}
+
+		public static long[] PreComputeHashes(
+			string T,
+			int P,
+			long p,
+			long x)
+		{
+			long[] H = new long[T.Length - P + 1];
+			H[H.Length - 1] = Q2HashingWithChain.PolyHash(T, T.Length - P, P, p, x);
+
+			long y = 1;
+			for (int i = 1; i <= P; i++)
+				checked
+				{
+					y = y * x % p;
+				}
+			for (int i = T.Length - P - 1; i >= 0; i--)
+				checked
+				{
+					H[i] = x * H[i + 1] + T[i] - (y * T[i + P]);
+					//does not calculate the negative numbers mod sth correctly
+					//so first we make it positive
+					while (H[i] < 0)
+						H[i] += p;
+
+					H[i] %= p;
+				}
+
+			return H;
+		}
+	}
+}
