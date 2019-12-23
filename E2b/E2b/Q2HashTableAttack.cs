1509c89f5e7f39c975f73c8d6c2570e8c110fff1
ghzamani <ghzamaninejad@gmail.com>
2019-12-23 14:49:02 +0330

    second part

---
+++ E2b/E2b/Q2HashTableAttack.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TestCommon;
+
+namespace E2b
+{
+    public class Q2HashTableAttack : Processor
+    {
+        public Q2HashTableAttack (string testDataName) : base(testDataName) 
+        {
+        }
+
+        public override string Process(string inStr)
+        {
+            long bucketCount = long.Parse(inStr);
+            return string.Join("\n", Solve(bucketCount));
+        }
+
+        public string[] Solve(long bucketCount)
+        {
+			double MaxLoadingFactor = 0.9;
+			string[] result = new string[(int) MaxLoadingFactor * bucketCount];
+
+			Random rnd = new Random();
+			int bucketNum = rnd.Next(1, (int)bucketCount);
+
+			for (int i = 0; i < result.Length; i++)
+			{
+				long val = (i + 1) * bucketCount + bucketNum;
+				
+			}
+			AllChars.OrderBy(x => x);
+			throw new NotImplementedException();            
+        }
+
+        #region Chars
+        static char[] LowChars = Enumerable
+            .Range(0, 26)
+            .Select(n => (char)('a' + n))
+            .ToArray();
+
+        static char[] CapChars = Enumerable
+            .Range(0, 26)
+            .Select(n => (char)('A' + n))
+            .ToArray();
+
+        static char[] Numbers = Enumerable
+            .Range(0, 10)
+            .Select(n => (char)('0' + n))
+            .ToArray();
+
+        static char[] AllChars = 
+            LowChars.Concat(CapChars).Concat(Numbers).ToArray();
+        #endregion
+
+
+        // پیاده‌سازی مورد استفاده دات‌نت برای پیدا کردن شماره باکت
+        // https://referencesource.microsoft.com/#mscorlib/system/collections/generic/dictionary.cs,bcd13bb775d408f1
+        public static long GetBucketNumber(string str, long bucketCount) =>
+            (str.GetHashCode() & 0x7FFFFFFF) % bucketCount;
+    }
+}
